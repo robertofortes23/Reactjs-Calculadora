da7d4f4f7f431d7dac3751a48bddf61857a03022
Roberto Fortes <robertbernd23@gmail.com>
2021-07-25 19:53:40 -0300

    Simulando botão

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -81,7 +81,24 @@ export default class Calculator extends Component {
     // const setOperation = op => this.setOperation(op)
     return (
       <div className="calculator">
-
+        <Button label="AC" click={this.clearMemory} triple />
+        {/* <Button label="AC" click={() => this.clearMemory()}/> */}
+        <Button label="/" click={this.setOperation} operation />
+        <Button label="7" click={this.addDigit} />
+        <Button label="8" click={this.addDigit} />
+        <Button label="9" click={this.addDigit} />
+        <Button label="*" click={this.setOperation} operation />
+        <Button label="4" click={this.addDigit} />
+        <Button label="5" click={this.addDigit} />
+        <Button label="6" click={this.addDigit} />
+        <Button label="-" click={this.setOperation} operation />
+        <Button label="1" click={this.addDigit} />
+        <Button label="2" click={this.addDigit} />
+        <Button label="3" click={this.addDigit} />
+        <Button label="+" click={this.setOperation} operation />
+        <Button label="0" click={this.addDigit} double />
+        <Button label="." click={this.addDigit} />
+        <Button label="=" click={this.setOperation} operation />
       </div>
     );
   }
