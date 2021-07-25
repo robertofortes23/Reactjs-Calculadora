0d21dd19732ea105caa99938db9bfb71728c9e9c
Roberto Fortes <robertbernd23@gmail.com>
2021-07-25 19:49:48 -0300

    Criando component calculator

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./Calculator.css";
+
+export default class Calculator extends Component {
+
+
+
+  render() {
+    // Garantem que o this vai estar correto
+    // const addDigit = n => this.addDigit(n)
+    // const setOperation = op => this.setOperation(op)
+    return (<div/>)
+  }
+}
